a21191aaeb5d66eae0e9beef25c437c56055c318
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-17 11:09:34 -0500

    primer commit

---
+++ microservicio-pasivo-plazofijo/src/main/java/nttdata/bootcamp/microservicios/pasivo/plazofijo/MicroservicioPasivoPlazofijoApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.pasivo.plazofijo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioPasivoPlazofijoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioPasivoPlazofijoApplication.class, args);
+	}
+
+}
---
+++ microservicio-pasivo-plazofijo/src/test/java/nttdata/bootcamp/microservicios/pasivo/plazofijo/MicroservicioPasivoPlazofijoApplicationTests.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.pasivo.plazofijo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioPasivoPlazofijoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
